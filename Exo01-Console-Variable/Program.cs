d8c7bcd6163d7ee9441355b4be55ed7afca1ffb1
Legrain Samuel <samuel.legrain@bstorm.be>
2024-04-10 13:54:54 +0200

    Ajoutez des fichiers projet.

---
+++ Exo01-Console-Variable/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+Console.WriteLine("Bonjour! Veuillez indiquer votre nom : ");
+string username = Console.ReadLine();
+Console.WriteLine($"Enchanté {username}!\nVeuillez introduire un premier nombre :");
+string firstNumber = Console.ReadLine();
+Console.WriteLine($"Ok {username}!\nMaintenant, introduisez un second nombre :");
+string secondNumber = Console.ReadLine();
+Console.WriteLine($"Le premier nombre est : {firstNumber} ; le second est : {secondNumber}.");
+Thread.Sleep( 1000 );  
+Console.WriteLine("✨Abracadabra!✨");
+string temp = firstNumber;
+firstNumber = secondNumber;
+secondNumber = temp;
+Console.WriteLine($"Maintenant, le premier nombre est : {firstNumber} ; le second est : {secondNumber}.");
