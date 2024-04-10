e80b986b1b0ccd9c4d6b820d7ff3b9db5a754d93
Legrain Samuel <samuel.legrain@bstorm.be>
2024-04-10 15:33:36 +0200

    Ajout Exo02 & Exo03

---
+++ Exo02-Parse/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Veuillez introduire un premier nombre :");
+string userInput = Console.ReadLine();
+int premierNombre = int.Parse(userInput);
+
+Console.Write("Veuillez introduire un second nombre :");
+int secondNombre = int.Parse(Console.ReadLine()) ;
+
+Console.WriteLine($"{premierNombre} + {secondNombre} = {premierNombre + secondNombre}");
+
+
+//Méthode dégueulasse : Pas clair, pas possible de ré-utiliser les valeurs
+//Console.WriteLine(int.Parse(Console.ReadLine()) + int.Parse(Console.ReadLine()));
+
+
+
---
+++ Exo03-TryParse/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Veuillez introduire un premier nombre :");
+string userInput = Console.ReadLine();
+int premierNombre;
+bool isOk = int.TryParse(userInput, out premierNombre);
+
+Console.Write("Veuillez introduire un second nombre :");
+int.TryParse(Console.ReadLine(), out int secondNombre);
+
+Console.WriteLine($"{premierNombre} + {secondNombre} = {premierNombre + secondNombre}");
