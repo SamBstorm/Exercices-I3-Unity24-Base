08fde7d30fc7f604536ba21ef7357155e6549b01
Legrain Samuel <samuel.legrain@bstorm.be>
2024-04-22 12:13:58 +0200

    Ajout Exo04 (p114)

---
+++ Exo4-NombrePairImpair/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Veuillez entrer votre nombre : ");
+int nbr = int.Parse(Console.ReadLine());
+if( (nbr/2) + (nbr/2) == nbr)
+{
+    Console.WriteLine($"Le nombre {nbr} est paire!");
+}
+else
+{
+    Console.WriteLine($"Le nombre {nbr} est impair.");
+}
