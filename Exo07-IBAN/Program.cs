7e181a0baba2da4820c57fbfd15d0933b1008435
Legrain Samuel <samuel.legrain@bstorm.be>
2024-04-23 14:39:21 +0200

    Ajout Exo07

--- Exo07-IBAN/Program.cs
+++ Exo07-IBAN/Program.cs
@@ -6,8 +6,14 @@ if (bban.Length == 12 && long.TryParse(bban, out _))
     long tenFirst = long.Parse(bban.Substring(0, 10));
     short twoLast = short.Parse(bban.Substring(10));
     short modulo = (short)(tenFirst % 97);
-    string result = (modulo == twoLast || (modulo == 0 && twoLast == 97))? "OK" : "KO" ;
+    string result = (modulo == twoLast || (modulo == 0 && twoLast == 97)) ? "OK" : "KO";
     Console.WriteLine(result);
+    if(result == "OK")
+    {
+        long code = long.Parse($"{twoLast}{twoLast}111400");
+        short moduloCode = (short)(98 - (code % 97));
+        Console.WriteLine($"BE{((moduloCode < 10)? "0" : "")}{moduloCode}{bban}");
+    }
 }
 else
 {
