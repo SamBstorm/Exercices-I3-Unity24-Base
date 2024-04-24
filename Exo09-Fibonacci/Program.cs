0b9179179976a73950f967a94ea6e9b14c878068
Legrain Samuel <samuel.legrain@bstorm.be>
2024-04-24 13:52:46 +0200

    Ajout Exo09 & Exo10

---
+++ Exo09-Fibonacci/Program.cs
@@ -0,0 +1,13 @@
+﻿int f0 = 0, f1 = 1;
+Console.WriteLine($"F0 : {f0}");
+Console.WriteLine($"F1 : {f1}");
+int fNext;
+int i = 2;
+while(i <= 25)          //Avec boucle FOR : for (int i = 2; i <= 25; i++)
+{
+    fNext = f0 + f1;
+    Console.WriteLine($"F{i} : {fNext}");
+    f0 = f1;
+    f1 = fNext;
+    i++;
+}
---
+++ Exo10-DoubleApproximatif/Program.cs
@@ -0,0 +1,9 @@
+﻿//for (double i = 0; i <= 20; i += 0.1)
+//{
+//    Console.WriteLine(Math.Round(i,4));
+//}
+
+for (decimal i = 0; i <= 20; i+= 0.1M)
+{
+    Console.WriteLine(i);
+}
