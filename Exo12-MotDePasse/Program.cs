aaf53f19fbec29e42830057f8428e296f4d77ff9
Legrain Samuel <samuel.legrain@bstorm.be>
2024-04-24 15:23:31 +0200

    Ajout Exo12 Mot de passe

---
+++ Exo12-MotDePasse/Program.cs
@@ -0,0 +1,18 @@
+﻿const string MDP = "Test1234=";
+string mdp;
+int count = 0;
+do
+{
+    Console.WriteLine("Entrer un mot de passe :");
+    mdp = Console.ReadLine().Trim();
+    count++;
+} while ( (mdp != MDP) && count < 3 );
+
+if (mdp == MDP)
+{
+    Console.WriteLine("Bienvenue, que puis-je faire pour vous?");
+}
+else
+{
+    Console.WriteLine("Ce compte est à présent bloqué.");
+}
