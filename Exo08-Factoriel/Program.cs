46c079f4de55aae6ddee51181bd7d89a55ecaa17
Legrain Samuel <samuel.legrain@bstorm.be>
2024-04-24 11:06:33 +0200

    Ajout Exo08 Factoriel

---
+++ Exo08-Factoriel/Program.cs
@@ -0,0 +1,21 @@
+﻿bool isConverted;
+int number;
+do
+{
+    Console.WriteLine("Veuillez indtoduire un nombre entier :");
+    string input = Console.ReadLine();
+    isConverted = int.TryParse(input, out number);
+} while (!isConverted);
+
+long result = 1;
+int i = 0;
+while (i < number)
+{
+    checked
+    {
+        result *= (number - i); //6 * 5 * 4 * 3 * 2 * 1
+    }
+    i++;
+}
+
+Console.WriteLine($"Le factoriel de {number} est {result}");
