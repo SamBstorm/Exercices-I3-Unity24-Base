e8c50cc2aeb6ac27e9e38e583ecd1d7ff48fdba9
Legrain Samuel <samuel.legrain@bstorm.be>
2024-04-24 14:15:14 +0200

    Ajout Exo11

---
+++ Exo11-TableMultiplication/Program.cs
@@ -0,0 +1,8 @@
+﻿for (int table = 1; table <= 5; table++)
+{
+    for (int multi = 1; multi <= 20; multi++)
+    {
+        Console.Write($"{table} x {multi} = {table * multi} ; ");
+    }
+    Console.WriteLine();
+}
