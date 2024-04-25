1b8befa94e119959ae5ddef6d30280643ac749de
Legrain Samuel <samuel.legrain@bstorm.be>
2024-04-25 10:18:53 +0200

    Ajout Exo14 et Exo15

---
+++ Exo14-PlusPetitNombre/Program.cs
@@ -0,0 +1,43 @@
+﻿#region Exemple simple
+//int number;
+//int min;
+//do
+//{
+//    Console.WriteLine("Veuillez indiquer un nombre :");
+//} while (!int.TryParse(Console.ReadLine(), out number));
+//min = number;
+//int count = 1;
+//do
+//{
+//    do
+//    {
+//        Console.WriteLine("Veuillez indiquer un nombre :");
+//    } while (!int.TryParse(Console.ReadLine(), out number));
+//    if (number < min)
+//    {
+//        min = number;
+//    }
+//    count++;
+//} while (count < 10);
+//Console.WriteLine($"La plus petite valeur est {min}."); 
+#endregion
+
+#region Exemple avec Null
+
+int number;
+int? min = null;
+
+int count = 0;
+do
+{
+    do
+    {
+        Console.WriteLine("Veuillez indiquer un nombre :");
+    } while (!int.TryParse(Console.ReadLine(), out number));
+    min ??= number;         //min = (min is null) ? number : min;
+    min = (number < min) ? number : min;
+    count++;
+} while (count < 10);
+Console.WriteLine($"La plus petite valeur est {min}.");
+
+#endregion
---
+++ Exo15-Code/Program.cs
@@ -0,0 +1,59 @@
+﻿#region Exemple avec boucle dans une boucle
+//uint nbRep, nbTiret, nbEspace;
+//string result = "";
+
+//do Console.WriteLine("Combien de fois le code doit être répété ?");
+//while (!uint.TryParse(Console.ReadLine(), out nbRep));
+
+//do Console.WriteLine("Combien de fois le code doit-il contenir des tirets ?");
+//while (!uint.TryParse(Console.ReadLine(), out nbTiret));
+
+//do Console.WriteLine("Combien de fois le code doit-il contenir des espaces blancs ?");
+//while (!uint.TryParse(Console.ReadLine(), out nbEspace));
+
+//for (int j = 0; j < nbRep; j++)
+//{
+//	for (int i = 0; i < nbTiret; i++)
+//	{
+//		result += "-";
+//	}
+
+//	for (int i = 0; i < nbEspace; i++)
+//	{
+//		result += " ";
+//	}
+//}
+
+//Console.WriteLine($"[{result}]"); 
+#endregion
+
+#region Exemple avec 3 boucles différentes
+uint nbRep, nbTiret, nbEspace;
+string result = "", code= "";
+
+do Console.WriteLine("Combien de fois le code doit être répété ?");
+while (!uint.TryParse(Console.ReadLine(), out nbRep));
+
+do Console.WriteLine("Combien de fois le code doit-il contenir des tirets ?");
+while (!uint.TryParse(Console.ReadLine(), out nbTiret));
+
+do Console.WriteLine("Combien de fois le code doit-il contenir des espaces blancs ?");
+while (!uint.TryParse(Console.ReadLine(), out nbEspace));
+
+for (int i = 0; i < nbTiret; i++)
+{
+	code += "-";
+}
+
+for (int i = 0; i < nbEspace; i++)
+{
+	code += " ";
+}
+
+for (int i = 0; i < nbRep; i++)
+{
+	result += code;
+}
+
+Console.WriteLine($"[{result}]");
+#endregion
