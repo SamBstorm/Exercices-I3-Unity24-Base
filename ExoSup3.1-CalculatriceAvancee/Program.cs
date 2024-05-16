0e2ebef04821d5146d8035e3e2f159da17d4bbf5
Legrain Samuel <samuel.legrain@bstorm.be>
2024-05-16 11:59:17 +0200

    Correction Exercices supplémentaires Bonus V2 (1.1 et 2.3)

---
+++ ExoSup3.1-CalculatriceAvancee/Program.cs
@@ -0,0 +1,50 @@
+﻿string operateur = null;
+double result = 0;
+do
+{
+    double nb;
+    do
+    {
+        Console.WriteLine("Veuillez indiquer un nombre :");
+    } while (!double.TryParse(Console.ReadLine(), out nb));
+
+    if(operateur is null)
+    {
+        result = nb;
+    }
+    else
+    {
+        switch (operateur)
+        {
+            case "+":
+                result += nb;
+                break;
+            case "-":
+                result -= nb;
+                break;
+            case "*":
+                result *= nb;
+                break;
+            case "/":
+                result /= nb;
+                break;
+        }
+    }
+
+    do
+    {
+        Console.WriteLine("Veuillez introduire un opérateur (+ - / * =) :");
+        operateur = Console.ReadLine();
+    } while (
+        operateur.Length != 1 ||
+        !(
+            operateur == "+" ||
+            operateur == "-" ||
+            operateur == "/" || 
+            operateur == "*" || 
+            operateur == "="
+        ));
+
+} while (operateur != "=");
+
+Console.WriteLine(result);
---
+++ ExoSup4.3-TableauPion/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+int position = 0;
+bool[] chemin = new bool[10];
+
+for (int i = 0; i < chemin.Length; i++)
+{
+    chemin[i] = false;
+}
+chemin[position] = true;
+
+string entry;
+
+do
+{
+    Console.Clear();
+    foreach (bool cell in chemin)
+    {
+        Console.Write((cell)?"😃":"-");
+    }
+    Console.WriteLine();
+
+    Console.WriteLine("Que voulez-vous faire ? (q) Gauche | (d) Droite | (x) Quitter");
+    entry = Console.ReadLine();
+    chemin[position] = false;
+    switch (entry)
+    {
+        case "q":
+            if (position != 0)
+            {
+                position--;
+            }
+            break;
+        case "d":
+            if (position != chemin.Length - 1)
+            {
+                position++;
+            }
+            break;
+    }
+    chemin[position] = true; 
+} while (entry != "x");
