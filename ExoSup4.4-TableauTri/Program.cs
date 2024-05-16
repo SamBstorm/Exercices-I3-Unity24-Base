460e4e70ba10b61ae875a2fad297841021c2805f
Legrain Samuel <samuel.legrain@bstorm.be>
2024-05-16 14:08:53 +0200

    Ajout Correction Exo supplémentaire Bonus V2 (2.4)

---
+++ ExoSup4.4-TableauTri/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] tableau = new int[10];
+
+Random RNG = new Random();
+
+for (int i = 0; i < tableau.Length; i++)
+{
+    tableau[i] = RNG.Next(0,101);
+}
+
+foreach (int cell in tableau)
+{
+    Console.Write($"[{cell}]");
+}
+Console.WriteLine();
+
+bool estTrie = false;
+
+for (int j = 0; j <= tableau.Length - 2 && !estTrie; j++)
+{
+    estTrie = true;
+    for (int i = 0; i <= tableau.Length - 2 - j; i++)
+    {
+        if (tableau[i] > tableau[i + 1])
+        {
+            int temp = tableau[i];
+            tableau[i] = tableau[i + 1];
+            tableau[i + 1] = temp;
+            estTrie = false;
+        }
+    } 
+
+    foreach (int cell in tableau)
+    {
+        Console.Write($"[{cell}]");
+    }
+    Console.WriteLine();
+}
