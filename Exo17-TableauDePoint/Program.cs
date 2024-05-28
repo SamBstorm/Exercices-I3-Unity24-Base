01526b54f288e96a62501a0884e1c448ef70b6cd
Samuel Legrain <samuel.legrain@bstorm.be>
2024-05-28 16:14:10 +0200

    Ajout Correction Exo 17 : Tableau de Point Nullable à deux dimensions

---
+++ Exo17-TableauDePoint/Program.cs
@@ -0,0 +1,34 @@
+﻿const int TAILLE = 5;
+
+Point?[,] tableau = new Point?[TAILLE,TAILLE];
+
+for (int i = 0; i < TAILLE; i++)
+{
+    Point p = new Point();
+    p.x = p.y = i + 1;
+    tableau[i, i] = p;
+}
+
+for (int ligne = 0; ligne < TAILLE; ligne++)
+{
+    for (int colone = 0; colone < TAILLE; colone++)
+    {
+        Point? cellule = tableau[ligne, colone];
+        if (cellule is null)
+        {
+            Console.Write("\t");
+        }
+        else
+        {
+            Console.Write($"X:{((Point)cellule).x} - Y:{cellule?.y}");
+        }
+    }
+    Console.WriteLine();
+}
+
+
+public struct Point
+{
+    public int x;
+    public int y;
+}
